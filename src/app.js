ce60fdf40e4250d48c16f7126c5583ffe14bf2e7
Lconforme <Lconforme06@yahoo.com>
2022-10-07 19:56:36 +0000

    cards done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function randomCard() {
+  let cardSuits = ["♣", "♦", "♥", "♠"];
+  let cardValue = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let selectedcardSuits =
+    cardSuits[Math.floor(Math.random() * cardSuits.length)];
+  document.getElementById("sign").innerHTML = selectedcardSuits;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  document.getElementById("rotator").innerHTML = selectedcardSuits;
+  let card = document.getElementById("card");
+  if (selectedcardSuits == "♥") {
+    card.classList.add("heart");
+  }
+  if (selectedcardSuits == "♦") {
+    card.classList.add("diamond");
+  }
+  if (selectedcardSuits == "♣") {
+    card.classList.add("clover");
+  }
+  if (selectedcardSuits == "♠") {
+    card.classList.add("spade");
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let selectedcardValue =
+    cardValue[Math.floor(Math.random() * cardValue.length)];
+  document.getElementById("number").innerHTML = selectedcardValue;
+}
+console.log(randomCard());
